509ad93d23a7518ab2e17ff14c1d871b1ef7b948
Александр <94863480+SAAAAAAnya@users.noreply.github.com>
2021-12-04 18:44:52 +0300

    Update Задание1.py

--- Задание1.py
+++ Задание1.py
@@ -22,14 +22,10 @@ res = {
 path = pathlib.Path("results")
 path.mkdir(exist_ok=True)
 file = path / "result_task1.json"
-# 2 способ out = open(file, "w")
 out = file.open("w")
-# 2 способ file = open("result.json", "w")
-# 2 способ file.write(json.dumps(res, indent=4))
 json.dump(res, out, indent=4)
 out.close()
 
 pylab.plot(x, y)
 pylab.grid()
-# pylab.show()
 pylab.savefig("results/task1.png")
